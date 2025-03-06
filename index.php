88a5256f9d329827fcf1555e7c211064dc7baa0f
Francisco Rubén Molero Rios <rubenmolero@outlook.es>
2025-03-06 11:44:44 +0100

    Restructured folders

--- index.php
+++ index.php
@@ -1,16 +1,16 @@
 <!DOCTYPE html>
 <html lang="es">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="./style/style.css" media="screen">
-    <title>All blue</title>
-</head>
-<body>
-    <h1>niarfeee</h1>
-    <?php 
-        phpinfo();
-    ?>
-    <script src="./js/main.js"></script>
-</body>
+    <head>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1.0">
+        <link rel="stylesheet" href="./style/style.css" media="screen">
+        <title>All blue</title>
+    </head>
+    <body>
+        <h1>niarfeee</h1>
+        <?php 
+            phpinfo();
+        ?>
+        <script src="./js/main.js"></script>
+    </body>
 </html>
