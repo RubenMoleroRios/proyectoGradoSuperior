f6ab2078540fa6eee222a1ca818abd75cabf38af
Francisco Rubén Molero Rios <rubenmolero@outlook.es>
2025-03-09 20:08:35 +0100

    database modificado

--- src/controllers/database.php
+++ src/controllers/database.php
@@ -1,5 +1,5 @@
 <?php
-    define("DATABASE","TiendaAnimales"); 
+    define("DATABASE","allblue"); 
     define("HOST", "localhost:3306");
     define("USER", "root");
     define("PASSWORD", "9NMJMDER");
