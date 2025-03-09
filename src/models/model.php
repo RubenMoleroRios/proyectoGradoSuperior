2f7e4017ecbc269f2265579a10ec74ed761060cc
Francisco Rubén Molero Rios <rubenmolero@outlook.es>
2025-03-09 21:35:44 +0100

    function getRoles ready

--- src/models/model.php
+++ src/models/model.php
@@ -1,5 +1,5 @@
 <?php
-include_once "database.php";
+include_once "../controllers/database.php";
 
 function crearConexion() {
     $conexion = mysqli_connect(HOST, USER, PASSWORD, DATABASE);
@@ -10,19 +10,21 @@ function crearConexion() {
     }
     return $conexion;
 }
-function getPosts() {
+
+function getRoles() {
     $db = crearConexion(DATABASE);
-    $sql = "SELECT * FROM article";
+    $sql = "SELECT * FROM rol_user";
     $result = mysqli_query($db, $sql);
     mysqli_close($db);
     if (mysqli_num_rows($result) > 0) {
         return $result;
+        $roles = [];
+        foreach ($result as &$rol) {
+            $roles[] = new RolUser($rol[RolUser::$ID],$rol[RolUser::$NAME]);
+        }
     } else {
         return "No hay posts";
     }
-
-    print("conexión hecha");
 }
-
 
 ?>
